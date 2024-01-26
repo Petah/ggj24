94df078c90604b8c993f0ae464bd132c32fd5cf1
David <d.c.neilsen@gmail.com>
2024-01-26 16:46:01 +1300

    Init

---
+++ browser/src/main.ts
@@ -0,0 +1,46 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+const ws = new WebSocket('ws://localhost:8080');
+console.log('ws', ws)
+
+ws.onopen = () => {
+  console.log('Connected to server');
+
+  ws.send('Hello, server!');
+}
+
+ws.onmessage = (event) => {
+  console.log(`Received message from server`, event);
+}
+
+ws.onclose = () => {
+  console.log('Disconnected from server');
+}
+
+ws.onerror = (error) => {
+  console.error(error);
+}
