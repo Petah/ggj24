38c6eda931ddbddfda9c26a7ad96278ff2186d8a
David <d.c.neilsen@gmail.com>
2024-01-27 15:59:56 +1300

    Move

--- server/src/game-map.ts
+++ server/src/game-map.ts
@@ -4,20 +4,30 @@ import * as PF from 'pathfinding';
 export class GameMap {
     public grid: PF.Grid;
     public finder: PF.AStarFinder;
+    public matrixString: string;
 
     constructor(
         public width: number,
         public height: number,
         public tiles: TileType[][],
     ) {
+        this.matrixString = ' ';
+        for (let i = 0; i < width; i++) {
+            this.matrixString += i % 10;
+        }
+        this.matrixString += '\n';
         const matrix = [];
         for (const row of tiles) {
             const matrixRow = [];
+            this.matrixString += matrix.length % 10;
             for (const tile of row) {
                 matrixRow.push(tile === TileType.WATER ? 1 : 0);
+                this.matrixString += tile === TileType.WATER ? '~' : 'X';
             }
             matrix.push(matrixRow);
+            this.matrixString += '\n';
         }
+        process.stdout.write(this.matrixString);
         this.grid = new PF.Grid(matrix);
         this.finder = new PF.AStarFinder({
             diagonalMovement: PF.DiagonalMovement.Never,
