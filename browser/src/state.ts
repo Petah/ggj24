c6c494dbad2abeec210bdf9dca0580c2859edaff
David <d.c.neilsen@gmail.com>
2024-01-27 15:14:20 +1300

    .

--- browser/src/state.ts
+++ browser/src/state.ts
@@ -2,8 +2,12 @@ import { GameState } from './events/game-list';
 
 export const state: {
     playerName: string;
+    cursorX: number;
+    cursorY: number;
     game?: GameState;
 } = {
     playerName: 'Test Player 1',
+    cursorX: parseInt(localStorage.getItem('cursorX') || '20'),
+    cursorY: parseInt(localStorage.getItem('cursorY') || '20'),
     game: undefined,
 };
